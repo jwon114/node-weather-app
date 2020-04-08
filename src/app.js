e6ffd81eeaeb9d88e693715edfaf99ac02ce0d06
jwon114 <jthgwong@gmail.com>
2020-04-08 17:16:00 +0100

    dev dependency nodemon, use weatherstack instead of darksky api

--- src/app.js
+++ src/app.js
@@ -49,11 +49,11 @@ app.get('/weather', (req, res) => {
     })
   }
 
-  geocode(req.query.address, (error, { latitude, longitude, location} = {}) => {
+  geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
     if (error) { 
       return res.send({ error })
     }
-      
+
     forecast(latitude, longitude, (error, forecastData) => {
       if (error) {
         return res.send({ error })
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,15 +1,15 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-  const url = 'https://api.darksky.net/forecast/b7a97566447704f11e4221de1cb9a299/' + latitude + ',' + longitude + '?units=si'
+  const url = 'http://api.weatherstack.com/current?access_key=8e2220a10a9b8fbdda4870bae719999e&query=' + latitude + ',' + longitude + '&units=m'
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
-      callback('Unable to conencto weather service!', undefined)
+      callback('Unable to connect to weather service!', undefined)
     } else if (body.error) {
       callback('Unable to find location!', undefined)
     } else {
-      callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees celsius out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+      callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees celsius out. It feels like ' + body.current.feelslike + '. There is a ' + body.current.precip.toFixed(1) + 'mm of rain. The humidity is ' + body.current.humidity + '%.')
     }
   })
 }
